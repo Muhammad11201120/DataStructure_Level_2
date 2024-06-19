196f7b780ae425da1dd5cbaaf20307e48690331b
Muhammed <muha.11201120@gmail.com>
2024-06-20 02:33:08 +0300

    new advanced data structures

---
+++ Trees_GeneralTree/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees_GeneralTree
+{
+    public class TreeNode<T>
+    {
+        public T value { get; set; }
+        public List<TreeNode<T>> Children { get; set; }
+
+        public TreeNode( T value )         
+        { 
+            this.value = value; 
+            this.Children = new List<TreeNode<T>>(); 
+        }
+        public void AddChild( TreeNode<T> child ) { Children.Add( child ); }
+    }
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; private set; }
+
+        public Tree( T root )
+        { 
+            this.Root = new TreeNode<T>(root); 
+        }
+    }
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            var companyTree = new Tree<string>( "CEO" );
+            var finance = new TreeNode<string>( "CFO" );
+            var tech = new TreeNode<string>( "CTO" );
+            var marketing = new TreeNode<string>( "CMO" );
+
+
+            companyTree.Root.AddChild( finance );
+            companyTree.Root.AddChild( tech );
+            companyTree.Root.AddChild( marketing );
+
+
+            finance.AddChild( new TreeNode<string>( "Accountant" ) );
+            tech.AddChild( new TreeNode<string>( "Developer" ) );
+            tech.AddChild( new TreeNode<string>( "UX Designer" ) );
+            marketing.AddChild( new TreeNode<string>( "Social Media Manager" ) );
+
+
+            PrintTree( companyTree.Root );
+            Console.ReadKey();
+        }
+        public static void PrintTree<T>( TreeNode<T> node, string indent = "" )
+        {
+            Console.WriteLine( indent + node.value );
+            foreach( var child in node.Children )
+            {
+                PrintTree( child, indent + "  " );
+            }
+        }
+    }
+}
---
+++ Trees_My_Family_Tree/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees_My_Family_Tree
+{
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; }
+
+        public TreeNode(T data)
+        {
+            this.Data = data;
+            this.Children = new List<TreeNode<T>>();
+        }
+        public void Add(TreeNode<T> node )
+        {
+            Children.Add( node );
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public Person( string name, string gender )
+        {
+            Name = name;
+            Gender = gender;
+        }
+    }
+    internal class Program
+    {
+        public static void PrintFamilyTree( TreeNode<Person> node, string indent = " " )
+        {
+            Console.WriteLine($"{indent}  ({node.Data.Name} - {node.Data.Gender})"  );
+
+            foreach( var child in node.Children )
+            {
+                PrintFamilyTree( child, indent + "   " );
+            }
+        }
+        static void Main( string[ ] args )
+        {
+            TreeNode<Person> ALI = new TreeNode<Person>( new Person( "ALI", "MALE" ) ); 
+            TreeNode<Person> Muhammed = new TreeNode<Person>( new Person( "Muhammed", "MALE" ) ); 
+            TreeNode<Person> RAWAN = new TreeNode<Person>( new Person( "Rawan", "MALE" ) ); 
+            TreeNode<Person> Ali = new TreeNode<Person>( new Person( "Ali", "MALE" ) ); 
+
+            ALI.Children.Add(Muhammed);
+            ALI.Children.Add(RAWAN);
+            Muhammed.Children.Add( Ali );
+
+            PrintFamilyTree( ALI );
+
+            Console.ReadKey();
+        }
+    }
+}
